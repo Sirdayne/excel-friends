d45f859b2a9aaeee55ceb57c83b8b5a508287c35
Yernar <yernar.mussin@nitka.com>
2020-05-31 13:02:27 +0600

    test-title

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,86 +1,89 @@
 <template lang="pug">
 #main-page
-  #forms
-    #form-menu
-      p(style="margin: 5px 5px 20px;") Добавить элемент меню:
-      el-form(label-width="120px")
-        el-form-item(label="Наименование")
-          el-input(v-model="menuName")
-        el-form-item(label="Цена")
-          el-input(v-model="menuPrice" type="number")
-        el-form-item
-          el-button(@click="addMenu") Добавить в меню
-    #form-friends
-      p(style="margin: 5px 5px 20px;") Добавить друга:
-      el-form(label-width="120px")
-        el-form-item(label="Имя")
-          el-input(v-model="friendName")
-        el-form-item
-          el-button(@click="addFriend") Добавить в таблицу
-
-  .container(v-if="menu && menu.length > 0")
-    .title Еда:
-    .menu
-      .menu-item(v-for="(item, index) in menu" :key="item.id")
-        p(style="margin-bottom: 8px;") {{ index + 1 }} позиция
-          el-button.btn-remove(@click="removeMenu(item.id)" size="mini") удалить
-        el-input(v-model="item.name")
-        el-input(v-model="item.price" type="number")
-
-  .container(v-if="friends && friends.length > 0")
-    .title Попали в эксельку:
-    .friends
-      .friend(v-for="(item, index) in friends" :key="item.id")
-        p(style="margin-bottom: 8px;") {{ index + 1 }} друг
-          el-button.btn-remove(@click="removeFriend(item.id)" size="mini") удалить
-        el-input(v-model="item.name")
-
-  .table-container#table
-    .table(v-if="menu && menu.length > 0 && friends && friends.length > 0" :style="{ width: getTableWidth + 'px'}")
-      .table-row.table-row_title
-        .table-cell Наименование | Цена
-        .table-cell(v-for="friend in friends" :key="friend.id") {{ friend.name }}
-        .table-cell
-      .table-row(v-for="row in menu" :key="row.id")
-        .table-cell {{ row.name }} | {{ row.price}}
-        .table-cell.cursor-pointer(
-          v-for="friend in friends"
-          :key="friend.id"
-          @click="updateFriendMenu(friend, row.id)"
-          :class="isMenuInFriend(friend, row.id) ? 'check-yes' : 'check-no'"
-          )
-          span {{ isMenuInFriend(friend, row.id) ? 'Да' : 'Нет' }}
-          span(style="margin-left: 10px;") {{ isMenuInFriend(friend, row.id) ? getDividedPrice(row.price, row.id) : 0 | noInfinityAndFixed }}
-        .table-cell
-      .table-row
-        .table-cell Итого
-        .table-cell(v-for="friend in friends" :key="friend.id") {{ getSum(friend.menus) | noInfinityAndFixed }}
-        .table-cell {{ getSumTotal() | noInfinityAndFixed }}
-      .table-row
-        .table-cell Итого с процентом
-          el-input.percent(v-model="percent" type="number" max="100" min="0")
-          span %
-        .table-cell(v-for="friend in friends" :key="friend.id") {{ getSumPercent(friend.menus) | noInfinityAndFixed }}
-        .table-cell {{ getSumTotalPercent() | noInfinityAndFixed }}
 
   el-dialog(title="Сохранить таблицу?"
-            :visible.sync="dialogSave")
+  :visible.sync="dialogSave")
     span Вы уверены что хотите перезаписать данные?
     span(slot="footer" class="dialog-footer")
       el-button(@click="dialogSave = false") Отмена
       el-button(type="primary" @click="saveToLocalStorage()") ОК
 
   el-dialog(title="Сохранить таблицу?"
-            :visible.sync="dialogLoad")
+  :visible.sync="dialogLoad")
     span Вы потеряете текущие значения в таблице.
     span(slot="footer" class="dialog-footer")
       el-button(@click="dialogLoad = false") Отмена
       el-button(type="primary" @click="loadFromLocalStorage()") ОК
 
-  .controls
-    el-button(@click="dialogSave = true") Сохранить таблицу
-    el-button(@click="dialogLoad = true") Загрузить таблицу
-    el-button(@click="saveImage") Сохранить картинку
+  .main-title Friends-excelka - Excel
+  .page-container
+    #forms
+      #form-menu
+        p(style="margin: 5px 5px 20px;") Добавить элемент меню:
+        el-form(label-width="120px")
+          el-form-item(label="Наименование")
+            el-input(v-model="menuName")
+          el-form-item(label="Цена")
+            el-input(v-model="menuPrice" type="number")
+          el-form-item
+            el-button(@click="addMenu") Добавить в меню
+      #form-friends
+        p(style="margin: 5px 5px 20px;") Добавить друга:
+        el-form(label-width="120px")
+          el-form-item(label="Имя")
+            el-input(v-model="friendName")
+          el-form-item
+            el-button(@click="addFriend") Добавить в таблицу
+
+    .container(v-if="menu && menu.length > 0")
+      .title Еда:
+      .menu
+        .menu-item(v-for="(item, index) in menu" :key="item.id")
+          p(style="margin-bottom: 8px;") {{ index + 1 }} позиция
+            el-button.btn-remove(@click="removeMenu(item.id)" size="mini") удалить
+          el-input(v-model="item.name")
+          el-input(v-model="item.price" type="number")
+
+    .container(v-if="friends && friends.length > 0")
+      .title Попали в эксельку:
+      .friends
+        .friend(v-for="(item, index) in friends" :key="item.id")
+          p(style="margin-bottom: 8px;") {{ index + 1 }} друг
+            el-button.btn-remove(@click="removeFriend(item.id)" size="mini") удалить
+          el-input(v-model="item.name")
+
+    .table-container#table
+      .table(v-if="menu && menu.length > 0 && friends && friends.length > 0" :style="{ width: getTableWidth + 'px'}")
+        .table-row.table-row_title
+          .table-cell Наименование | Цена
+          .table-cell(v-for="friend in friends" :key="friend.id") {{ friend.name }}
+          .table-cell
+        .table-row(v-for="row in menu" :key="row.id")
+          .table-cell {{ row.name }} | {{ row.price}}
+          .table-cell.cursor-pointer(
+            v-for="friend in friends"
+            :key="friend.id"
+            @click="updateFriendMenu(friend, row.id)"
+            :class="isMenuInFriend(friend, row.id) ? 'check-yes' : 'check-no'"
+            )
+            span {{ isMenuInFriend(friend, row.id) ? 'Да' : 'Нет' }}
+            span(style="margin-left: 10px;") {{ isMenuInFriend(friend, row.id) ? getDividedPrice(row.price, row.id) : 0 | noInfinityAndFixed }}
+          .table-cell
+        .table-row
+          .table-cell Итого
+          .table-cell(v-for="friend in friends" :key="friend.id") {{ getSum(friend.menus) | noInfinityAndFixed }}
+          .table-cell {{ getSumTotal() | noInfinityAndFixed }}
+        .table-row
+          .table-cell Итого с процентом
+            el-input.percent(v-model="percent" type="number" max="100" min="0")
+            span %
+          .table-cell(v-for="friend in friends" :key="friend.id") {{ getSumPercent(friend.menus) | noInfinityAndFixed }}
+          .table-cell {{ getSumTotalPercent() | noInfinityAndFixed }}
+
+    .controls
+      el-button(@click="dialogSave = true") Сохранить таблицу
+      el-button(@click="dialogLoad = true") Загрузить таблицу
+      el-button(@click="saveImage") Сохранить картинку
 
 </template>
 
